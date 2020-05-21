29399999b29c49baebf350b69b94ed3250c1dac6
sendtodilip <sendtodilip>
2020-05-22 00:51:50 +0530

    Initial Commit (Routing Basics and Features)

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -14,12 +14,20 @@ import { PageNotFoundComponent } from './page-not-found.component';
 import { ProductModule } from './products/product.module';
 import { UserModule } from './user/user.module';
 import { MessageModule } from './messages/message.module';
+import { RouterModule } from '@angular/router';
+import { ProductListComponent } from './products/product-list.component';
+import { LoginComponent } from './user/login.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     HttpClientModule,
     InMemoryWebApiModule.forRoot(ProductData, { delay: 1000 }),
+    RouterModule.forRoot([
+      {path: 'welcome', component: WelcomeComponent},
+      {path: '', redirectTo: 'welcome', pathMatch: 'full'},
+      {path: '**', component: PageNotFoundComponent}
+    ]),
     ProductModule,
     UserModule,
     MessageModule
