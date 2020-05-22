43e8bdff797e57d703d02c73a8a5cca0c7a69cb1
sendtodilip <sendtodilip>
2020-05-22 18:12:36 +0530

    Routing Parameters

--- APM-Start/src/app/products/product-edit/product-edit.component.ts
+++ APM-Start/src/app/products/product-edit/product-edit.component.ts
@@ -1,22 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { MessageService } from '../../messages/message.service';
 
 import { Product } from '../product';
 import { ProductService } from '../product.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   templateUrl: './product-edit.component.html',
   styleUrls: ['./product-edit.component.css']
 })
-export class ProductEditComponent {
+export class ProductEditComponent implements OnInit {
   pageTitle = 'Product Edit';
   errorMessage: string;
 
   product: Product;
 
   constructor(private productService: ProductService,
-              private messageService: MessageService) { }
+              private messageService: MessageService,
+              private route: ActivatedRoute,
+              private router: Router) { }
+
+  ngOnInit(): void {
+    // const id = +this.route.snapshot.paramMap.get('id');
+    // this.getProduct(id);
+    this.route.paramMap.subscribe(
+      params => {
+        const id = +params.get('id');
+        this.getProduct(id);
+      }
+    )
+  }
 
   getProduct(id: number): void {
     this.productService.getProduct(id).subscribe({
@@ -77,5 +91,6 @@ export class ProductEditComponent {
     }
 
     // Navigate back to the product list
+    this.router.navigate(['./products']);
   }
 }
